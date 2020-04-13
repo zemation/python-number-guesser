a1a866508e4b90a8f4e869ed598e8678904cbedf
RB Rodgers <rbrandon@zemation.com>
2020-04-13 04:26:02 +0000

    reworking into functions

--- guesser.py
+++ guesser.py
@@ -8,13 +8,40 @@ print('************************************')
 print()
 
 random_number = random.randrange(1, 21)
-user_guess = ''
+
+def get_user_guess():
+  user_guess = int(input('Guess a number between 1 and 20 (q to quit): '))
+  return user_guess
+
+def qualify_guess(guess):
+  if guess == 'q':
+    exit()
+  elif (guess <= 0) or (guess > 20):
+    qualify_guess(guess)
+  else:
+    return guess
+
+qualify_guess(get_user_guess())
+
+
+"""  elif (guess <= 0) or (guess > 20):
+    return 'Please enter a number between 1 and 20'
+  else:
+    return determine_number(guess) 
+
+def determine_number(guess):
+  while guess != random_number:
+    if user_guess > guess
+
 
 while user_guess != random_number:
-  user_guess = int(input('Guess a number between 1 and 20: '))
+  if user_guess <= 0 or user_guess
   if user_guess > random_number:
     print('Your guess is too high.')
   elif user_guess < random_number:
     print('Your guess is too low.')
 else: 
   print(f'You guessed correctly with the number {random_number}.')
+"""
+
+
